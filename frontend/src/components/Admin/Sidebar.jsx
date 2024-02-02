cdcb4b67dbdbb63af238c4860a04e432d846f75f
Hafsah Naseer <130146153+Hafsah2916@users.noreply.github.com>
2024-02-02 13:11:07 +0530

    changes

--- frontend/src/components/Admin/Sidebar.jsx
+++ frontend/src/components/Admin/Sidebar.jsx
@@ -1,10 +1,40 @@
+import { BsCart, BsCart3 } from "react-icons/bs"
+import { Link } from "react-router-dom"
 
 const Sidebar = () => {
   return (
     <>
-    
-    
-    
+
+      <aside>
+        <div className="">
+        <div className="flex">
+          <BsCart3 />SHOP
+        </div>
+        <span className="">X</span>
+        </div>
+        <ul>
+          <li>
+            <Link to="/">Dashboard</Link>
+
+          </li>
+          <li>
+            <Link to="/">Customers</Link>
+
+          </li>
+          <li>
+            <Link to="/">Manage Product</Link>
+
+          </li>
+          <li>
+            <Link to="/">Add Product</Link>
+
+          </li>
+        
+        
+
+        </ul>
+      </aside>
+
     </>
   )
 }
